133dd43dedba5df49049cf91147aff23f08d798d
Simpli-Code <74283859+Simpli-Code@users.noreply.github.com>
2024-03-06 08:43:41 +0100

    Add files via upload

---
+++ src/main/main.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chruhin <chruhin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/12 10:00:39 by chruhin           #+#    #+#             */
+/*   Updated: 2023/10/12 10:07:58 by chruhin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("file.txt", O_RDONLY);
+	while (42)
+	{
+		line = get_next_line(fd);
+		printf("%s", line);
+		if (line == NULL)
+			break ;
+		free(line);
+	}
+	close(fd);
+	return (0);
+}
---
+++ src/utils/get_next_line_utils.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chruhin <chruhin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/17 11:58:37 by chruhin           #+#    #+#             */
+/*   Updated: 2024/03/05 13:14:38 by chruhin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr(const char *s, unsigned int position, size_t len)
+{
+	size_t	i;
+	size_t	slen;
+	char	*substr;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	slen = ft_strlen(s);
+	if (position >= slen)
+		return (NULL);
+	if (len >= slen)
+		len = slen - position;
+	substr = malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);
+	while (i < len)
+		substr[i++] = s[position++];
+	substr[i] = '\0';
+	return (substr);
+}
